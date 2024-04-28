967bf2565b78af2f1bd0c062fadd367b894acfbe
qpan <qpan17@gmail.com>
2024-04-28 16:49:47 -0400

    fixed typo in metadata.description and gitignore jetBrains files and folders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Providers from '@/app/providers';
+import React from "react";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Qian Pan',
-  description: 'A Frontend Developer Portolio',
+  description: 'A Frontend Developer Portfolio',
   icons: {
     icon: '/images/favicon.ico',
   },
